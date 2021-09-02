072cbc10263836839b817bf92febbaddfb078bf4
Madhapur Nitish Kumar <71878747+mnk17arts@users.noreply.github.com>
2021-09-02 22:57:32 +0530

    Create _head.php

---
+++ bits/_head.php
@@ -0,0 +1,20 @@
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>WELCOME TO MLINKS</title>
+    
+    <!-- css -->
+    <link href="bits/styles.css" rel="stylesheet">
+    <link rel="shortcut icon" href="bits/favicon.ico" type="image/ico">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.11.0/css/jquery.dataTables.min.css">
+</head>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat+Alternates:ital,wght@1,500&display=swap');
+</style>
