83171c6e2e06e7a4e72b71a646d49cdf24a84c11
Madhapur Nitish Kumar <71878747+mnk17arts@users.noreply.github.com>
2021-09-02 22:43:53 +0530

    Create _footer.php

---
+++ bits/_footer.php
@@ -0,0 +1,10 @@
+<?php
+
+echo '
+<div class="container-fluid bg-dark text-light pt-2 p-1">
+<p class="text-center"> Copyright <i style="color:orangered;font-family: \'Montserrat Alternates\', sans-serif;"><u>mlinks</u></i> 2021 | All rights reserved</p>
+</div>
+';
+
+
+?>
