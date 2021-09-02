161d984b7bf06492d1abfe924f4eede8e8fa08c1
Madhapur Nitish Kumar <71878747+mnk17arts@users.noreply.github.com>
2021-09-02 23:06:25 +0530

    Create about.php

---
+++ about.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+
+<?php include 'bits/_head.php' ?>
+
+<body>
+    <?php require 'bits/_connectdb.php' ?>
+    <?php require 'bits/_navbar.php' ?>
+
+  <h1> LOADING... </h1>
+
+
+    <?php require 'bits/_footer.php' ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+    <script>
+        document.getElementById("nb-about").classList.add("active");
+    </script>
+
+</body>
+
+</html>
